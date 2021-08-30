2572c6af523352c38971e3b4e7f47447b1a1b5e8
luckinamargo <luckinamargo@gmail.com>
2021-08-30 15:47:33 +0300

    fix syntax

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,76 @@
 exports = typeof window === 'undefined' ? global : window;
 
-exports.arraysAnswers = {
-    indexOf: function(arr, item) {
+const indexOf = function(arr, item) {
 
-    },
-
-    sum: function(arr) {
-
-    },
-
-    remove: function(arr, item) {
-
-    },
-
-    removeWithoutCopy: function(arr, item) {
-
-    },
-
-    append: function(arr, item) {
-
-    },
-
-    truncate: function(arr) {
-
-    },
-
-    prepend: function(arr, item) {
-
-    },
-
-    curtail: function(arr) {
-
-    },
-
-    concat: function(arr1, arr2) {
-
-    },
-
-    insert: function(arr, item, index) {
-
-    },
-
-    count: function(arr, item) {
-
-    },
-
-    duplicates: function(arr) {
-
-    },
-
-    square: function(arr) {
-
-    },
-
-    findAllOccurrences: function(arr, target) {
-
-    }
 };
+
+const sum = function(arr) {
+
+};
+
+const remove = function(arr, item) {
+
+};
+
+const removeWithoutCopy = function(arr, item) {
+
+};
+
+const append = function(arr, item) {
+
+};
+
+const truncate = function(arr) {
+
+};
+
+const prepend = function(arr, item) {
+
+};
+
+const curtail = function(arr) {
+
+};
+
+const concat = function(arr1, arr2) {
+
+};
+
+const insert = function(arr, item, index) {
+
+};
+
+const count = function(arr, item) {
+
+};
+
+const duplicates = function(arr) {
+
+};
+
+const square = function(arr) {
+
+};
+
+const findAllOccurrences = function(arr, target) {
+
+};
+
+const arraysAnswers = {
+    indexOf,
+    sum,
+    remove,
+    removeWithoutCopy,
+    append,
+    truncate,
+    prepend,
+    curtail,
+    concat,
+    insert,
+    count,
+    duplicates,
+    square,
+    findAllOccurrences
+};
+
+exports.asyncAnswers = arraysAnswers;
