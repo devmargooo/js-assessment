dc26ed5ab1ff76b7b71deb029338864b7d82a71b
luckinamargo <luckinamargo@gmail.com>
2021-09-02 11:05:00 +0300

    async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,20 @@
 exports = typeof window === 'undefined' ? global : window;
 
-exports.asyncAnswers = {
-  async: function(value) {
-
-  },
-
-  manipulateRemoteData: function(url) {
-
-  }
+const async = function(value) {
+  return Promise.resolve(value)
 };
+
+const manipulateRemoteData = function(url) {
+  return fetch(url)
+      .then(result => result.json())
+      .then(data => data.people
+          .map(item => item.name)
+          .sort())
+};
+
+const asyncAnswers = {
+  async,
+  manipulateRemoteData,
+};
+
+exports.asyncAnswers = asyncAnswers;
