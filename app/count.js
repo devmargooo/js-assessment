31c19a8ad8cc9fae26fd198b963b530c7d2053be
luckinamargo <luckinamargo@gmail.com>
2021-09-03 18:20:36 +0300

    count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+    count: function (start, end) {
+        let initial = start;
 
-  }
+        const timer = setInterval(tick, 100);
+        tick();
+        function tick () {
+            console.log(initial);
+            initial++;
+            if (initial > end) {
+                clearInterval(timer);
+            }
+        }
+
+        return {
+            cancel: () => clearInterval(timer)
+        }
+    }
 };
