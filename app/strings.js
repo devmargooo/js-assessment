895d3c9a841fe81910d612692e93071521536fc3
luckinamargo <luckinamargo@gmail.com>
2021-09-06 22:44:24 +0300

    reduceString

--- app/strings.js
+++ app/strings.js
@@ -1,9 +1,24 @@
 exports = typeof window === 'undefined' ? global : window;
 
-exports.stringsAnswers = {
-  reduceString: function(str, amount) {
+const reduceString = function(str, amount) {
+  let array = str.split('');
+  let index = 1;
+  let counter = 1;
+  while (index < array.length) {
+    const shoud_reduce = array[index] === array[index - 1];
+    if (shoud_reduce && counter >= amount) {
+      array.splice(index, 1);
+      continue;
+    }
+    counter = shoud_reduce ? counter + 1 : 1;
+    index++;
 
-  },
+  }
+  return array.join('');
+};
+
+exports.stringsAnswers = {
+  reduceString,
 
   wordWrap: function(str, cols) {
 
