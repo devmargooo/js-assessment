19aad7bb15c8219f360118df72beaf137a1c5a2a
luckinamargo <luckinamargo@gmail.com>
2021-09-13 19:04:16 +0300

    logicalOperators, functions

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn.apply(window, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   functionFunction: function(str) {
-
+      return function (new_str) {
+          return str + ', ' + new_str;
+      }
   },
 
   makeClosures: function(arr, fn) {
-
+    const res = [];
+    for (let i = 0; i < arr.length; i++) {
+      res.push(fn.bind(null, arr[i]));
+    }
+    for (let i = 0; i < arr.length; i++) {
+    }
+    return res;
   },
 
   partial: function(fn, str1, str2) {
--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.logicalOperatorsAnswers = {
   or: function(a, b) {
-
+    switch (true) {
+      case (!!a && !!b): return true;
+      case (!!a && !b): return true;
+      case (!a && !!b): return true;
+      case (!a && !b): return false;
+    }
   },
 
   and: function(a, b) {
-
+    switch (true) {
+      case (!!a && !!b): return true;
+      case (!!a && !b): return false;
+      case (!a && !!b): return false;
+      case (!a && !b): return false;
+    }
   }
 };
