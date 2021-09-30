7a06f255a6f2dd44eb3e719b50812ca036bb0248
luckinamargo <luckinamargo@gmail.com>
2021-09-30 22:19:55 +0900

    recursions

--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,47 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+    listFiles: function(data, dirName) {
+        let result = [];
+        const node = dirName ? findNode(data, dirName) : data;
+        list(node);
+        function findNode(start, target) {
+            const queue = [start];
+            while (queue.length) {
+                const current = queue.shift();
+                if (current.dir === target) {
+                    return current;
+                }
+                const files = current.files;
+                for (let i = 0; i < files.length; i++) {
+                    if (files[i].dir) {
+                        queue.push(files[i])
+                    }
+                }
+            }
+        }
+        function list(node) {
+            const files = node.files;
+            for (let i = 0; i < files.length; i++) {
+                if (!files[i].dir) {
+                    result.push(files[i]);
+                } else {
+                    list(files[i]);
+                }
+            }
+        }
+        return result;
+    },
 
-  },
+    permute: function(arr) {
 
-  permute: function(arr) {
+    },
 
-  },
+    fibonacci: function(n) {
 
-  fibonacci: function(n) {
+    },
 
-  },
+    validParentheses: function(n) {
 
-  validParentheses: function(n) {
-
-  }
+    }
 };
